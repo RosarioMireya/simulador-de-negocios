65a2c92721cb275c86341bd79984c260535733e4
Jhonatan Condarco <jhon@MacBook-de-Jhonatan.local>
2018-07-30 17:47:22 -0400

    google login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   email: string;
   password: string;
+  
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private _firebaseAuth: AngularFireAuth) {}
 
   signup() {
     this.authService.signup(this.email, this.password);
@@ -21,8 +27,15 @@ export class AppComponent {
     this.authService.login(this.email, this.password);
     this.email = this.password = '';    
   }
+  signInWithGoogle() {
+    return this._firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    )
+  }
 
   logout() {
     this.authService.logout();
+    this._firebaseAuth.auth.signOut()
+
   }
 }
