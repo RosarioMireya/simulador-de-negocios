31168e1c2216249d19ef43e1aa6ffb3a8838ebf2
Jhonatan Condarco <jhon@MacBook-de-Jhonatan.local>
2018-08-16 21:25:24 -0400

    pestañas

--- src/app/tabla-de-decision/tabla-de-decision.component.ts
+++ src/app/tabla-de-decision/tabla-de-decision.component.ts
@@ -10,8 +10,33 @@ export class TablaDeDecisionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.decision();
   }
   iniciar(){
     alert("Iniciando inversion, preparando primer bimestre");
+  }
+  decision(){
+    document.getElementById("decisiones").style.display="block";
+    document.getElementById("tabla-analisis").style.display="none";
+    document.getElementById("informe").style.display="none";
+    document.getElementById("analisis").style.display="none";
+  }
+  tanalisis(){
+    document.getElementById("decisiones").style.display="none";
+    document.getElementById("tabla-analisis").style.display="block";
+    document.getElementById("informe").style.display="none";
+    document.getElementById("analisis").style.display="none";
+  }
+  informe(){
+    document.getElementById("decisiones").style.display="none";
+    document.getElementById("tabla-analisis").style.display="none";
+    document.getElementById("informe").style.display="block";
+    document.getElementById("analisis").style.display="none";
+  }
+  analisis(){
+    document.getElementById("decisiones").style.display="none";
+    document.getElementById("tabla-analisis").style.display="none";
+    document.getElementById("informe").style.display="none";
+    document.getElementById("analisis").style.display="block";
   }
 }
