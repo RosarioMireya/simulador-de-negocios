66cc822760330882a746b3ec2776ef5b312c1d7b
IvySaskia <saskiaivy@outlook.com>
2018-08-31 01:02:39 -0400

    31agosto

--- src/app/tabla-de-decision/tabla-de-decision.component.ts
+++ src/app/tabla-de-decision/tabla-de-decision.component.ts
@@ -46,4 +46,154 @@ export class TablaDeDecisionComponent implements OnInit {
     document.getElementById("titulo").innerText="ANALISIS";
 
   }
+
+
+  produccionvsventas(){
+    document.getElementById("produccionvsventasID").style.display="block";
+    document.getElementById("costovsprecioID").style.display="none";
+    document.getElementById("capacidadvsproduccionID").style.display="none";
+    document.getElementById("companiavscompetenciaID").style.display="none";
+    document.getElementById("consejosID").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS";
+
+  }
+  costovsprecio(){
+    document.getElementById("produccionvsventasID").style.display="none";
+    document.getElementById("costovsprecioID").style.display="block";
+    document.getElementById("capacidadvsproduccionID").style.display="none";
+    document.getElementById("companiavscompetenciaID").style.display="none";
+    document.getElementById("consejosID").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS";
+
+  }
+  capacidadvsproduccion(){
+    document.getElementById("produccionvsventasID").style.display="none";
+    document.getElementById("costovsprecioID").style.display="none";
+    document.getElementById("capacidadvsproduccionID").style.display="block";
+    document.getElementById("companiavscompetenciaID").style.display="none";
+    document.getElementById("consejosID").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS";
+
+  }
+  companiavscompetencia(){
+    document.getElementById("produccionvsventasID").style.display="none";
+    document.getElementById("costovsprecioID").style.display="none";
+    document.getElementById("capacidadvsproduccionID").style.display="none";
+    document.getElementById("companiavscompetenciaID").style.display="block";
+    document.getElementById("consejosID").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS";
+
+  }
+  consejos(){
+    document.getElementById("produccionvsventasID").style.display="none";
+    document.getElementById("costovsprecioID").style.display="none";
+    document.getElementById("capacidadvsproduccionID").style.display="none";
+    document.getElementById("companiavscompetenciaID").style.display="none";
+    document.getElementById("consejosID").style.display="block";
+    document.getElementById("titulo").innerText="ANALISIS";
+
+  }
+
+
+  EstadoDeResultados(){
+    document.getElementById("EstadoDeResultadosID").style.display="block";
+    document.getElementById("BalanceGeneralID").style.display="none";
+    document.getElementById("VentasID").style.display="none";
+    document.getElementById("ProduccionID").style.display="none";
+    document.getElementById("CompaniaID").style.display="none";
+    document.getElementById("titulo").innerText="INFORMES DE LA COMPAÑIA";
+
+  }
+
+  BalanceGeneral(){
+    document.getElementById("EstadoDeResultadosID").style.display="none";
+    document.getElementById("BalanceGeneralID").style.display="block";
+    document.getElementById("VentasID").style.display="none";
+    document.getElementById("ProduccionID").style.display="none";
+    document.getElementById("CompaniaID").style.display="none";
+    document.getElementById("titulo").innerText="INFORMES DE LA COMPAÑIA";
+
+  }
+  Ventas(){
+    document.getElementById("EstadoDeResultadosID").style.display="none";
+    document.getElementById("BalanceGeneralID").style.display="none";
+    document.getElementById("VentasID").style.display="block";
+    document.getElementById("ProduccionID").style.display="none";
+    document.getElementById("CompaniaID").style.display="none";
+    document.getElementById("titulo").innerText="INFORMES DE LA COMPAÑIA";
+
+  }
+
+  Produccion(){
+    document.getElementById("EstadoDeResultadosID").style.display="none";
+    document.getElementById("BalanceGeneralID").style.display="none";
+    document.getElementById("VentasID").style.display="none";
+    document.getElementById("ProduccionID").style.display="block";
+    document.getElementById("CompaniaID").style.display="none";
+    document.getElementById("titulo").innerText="INFORMES DE LA COMPAÑIA";
+
+  }
+
+  Compania(){
+    document.getElementById("EstadoDeResultadosID").style.display="none";
+    document.getElementById("BalanceGeneralID").style.display="none";
+    document.getElementById("VentasID").style.display="none";
+    document.getElementById("ProduccionID").style.display="none";
+    document.getElementById("CompaniaID").style.display="block";
+    document.getElementById("titulo").innerText="INFORMES DE LA COMPAÑIA";
+
+  }
+
+
+  VisionGeneral(){
+    document.getElementById("VisionGeneralID").style.display="block";
+    document.getElementById("ProduccionTablaAnalisisID").style.display="none";
+    document.getElementById("VentasTablaAnalisisID").style.display="none";
+    document.getElementById("EconomiaID").style.display="none";
+    document.getElementById("PorcentajeMercado").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS DE LA INDUSTRIA";
+
+  }
+
+  ProduccionTablaAnalisis(){
+    document.getElementById("VisionGeneralID").style.display="none";
+    document.getElementById("ProduccionTablaAnalisisID").style.display="block";
+    document.getElementById("VentasTablaAnalisisID").style.display="none";
+    document.getElementById("EconomiaID").style.display="none";
+    document.getElementById("PorcentajeMercado").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS DE LA INDUSTRIA";
+
+  }
+
+  VentasTablaAnalisis(){
+    document.getElementById("VisionGeneralID").style.display="none";
+    document.getElementById("ProduccionTablaAnalisisID").style.display="none";
+    document.getElementById("VentasTablaAnalisisID").style.display="block";
+    document.getElementById("EconomiaID").style.display="none";
+    document.getElementById("PorcentajeMercado").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS DE LA INDUSTRIA";
+
+  }
+
+  Economia(){
+    document.getElementById("VisionGeneralID").style.display="none";
+    document.getElementById("ProduccionTablaAnalisisID").style.display="none";
+    document.getElementById("VentasTablaAnalisisID").style.display="none";
+    document.getElementById("EconomiaID").style.display="block";
+    document.getElementById("PorcentajeMercado").style.display="none";
+    document.getElementById("titulo").innerText="ANALISIS DE LA INDUSTRIA";
+
+  }
+
+  PorcentajeMercado(){
+    document.getElementById("VisionGeneralID").style.display="none";
+    document.getElementById("ProduccionTablaAnalisisID").style.display="none";
+    document.getElementById("VentasTablaAnalisisID").style.display="none";
+    document.getElementById("EconomiaID").style.display="none";
+    document.getElementById("PorcentajeMercado").style.display="block";
+    document.getElementById("titulo").innerText="ANALISIS DE LA INDUSTRIA";
+
+  }
+
+
 }
